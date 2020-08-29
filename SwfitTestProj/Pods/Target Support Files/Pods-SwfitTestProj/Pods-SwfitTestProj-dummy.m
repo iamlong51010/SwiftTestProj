c08f7820efbd0fc61c2dd6e6fdde36a03b87da1b
iamlong51010 <iamlong510@163.com>
2020-08-29 12:04:17 +0800

    1) 完成Codable协议,api调用后获取到的json字符串解析到自己定义的struct数据结构里 2) 在api调用里增加combine机制，使用RxSwift 3) 应用内字符串多语言化使用苹果原生Localization方式 1) Modify Json String to struct from api call using protocol Codable 2) add combine mechanism in api call using RxSwift 3) using Apple Original Localization for multi-language strings in the app

---
+++ SwfitTestProj/Pods/Target Support Files/Pods-SwfitTestProj/Pods-SwfitTestProj-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwfitTestProj : NSObject
+@end
+@implementation PodsDummy_Pods_SwfitTestProj
+@end
---
+++ SwfitTestProj/Pods/Target Support Files/Pods-SwfitTestProjUITests/Pods-SwfitTestProjUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwfitTestProjUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwfitTestProjUITests
+@end
